ac99869557289efe726dbc0b3ced0ebb36310854
Chandler Bowman <doctorgrant99@gmail.com>
2023-03-30 17:13:42 -0600

    new router path

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,17 +1,44 @@
 <template>
-    <div class="component">
-
+    <div class="container-fluid">
+        <section class="row pt-3">
+            <div class="col-9 text-center">
+                <img :src="profile?.coverImg" :alt="profile?.name">
+            </div>
+        </section>
 
     </div>
 </template>
 
 
 <script>
-import { AppState } from '../AppState';
+import { AppState } from '../AppState.js';
 import { computed, reactive, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { logger } from '../utils/Logger.js';
+import { profilesService } from '../services/ProfilesService.js';
+import Pop from '../utils/Pop.js';
+import { blogsService } from '../services/BlogsService.js';
+
 export default {
     setup(){
-    return {  }
+        const route = useRoute()
+
+        async function getProfileById() {
+            try {
+                const profileId = route.params.profileId
+                await profilesService.getProfileById(profileId)
+            } catch (error) {
+                logger.log(error.message)
+                Pop.error(error.message)
+            }
+        }
+        onMounted(() => {
+            getProfileById()
+        })
+    return {
+        route,
+        profile: computed(() => AppState.activeProfile)
+    }
     }
 };
 </script>
